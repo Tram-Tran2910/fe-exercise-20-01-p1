076a94decf5dc44585dadb5731f4bcb32d8d82bd
TramTNCE190848 <tram85038@gmail.com>
2026-01-20 10:28:00 +0700

    add route structure and page components (Home, About, Contact)

---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <>
+      <h1>Đây là trang thông tin</h1>
+    </>
+  );
+}
+
+export default About;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,9 @@
+function Contact() {
+  return (
+    <>
+      <h1>Đây là trang liên hệ</h1>
+    </>
+  );
+}
+
+export default Contact;
